63af3b58f9850270edc46fe353a5e25794c6c986
piptouque <63.pthiel@gmail.com>
2021-01-17 13:58:19 +0100

    [Improvement]: Added pytorch_ssim and related info.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # image-interpolation
 Deep learning project to create new images based on the previous ones
+
+### Miscellaneous information
+
+Using :
+
+* Pytorch project template at: https://github.com/victoresque/pytorch-template
+
+* Pytorch implementation of SSIM : https://github.com/Po-Hsun-Su/pytorch-ssim
--- model/loss.py
+++ model/loss.py
@@ -1,9 +1,30 @@
 import torch.nn.functional as F
 import numpy as np
 
+import torchvision
+import pytorch_ssim
+
 def nll_loss(output, target):
     return F.nll_loss(output, target)
 
 def nsrr_loss(output: np.ndarray, target: np.ndarray) -> float:
     # todo
     pass
+
+class VGGLoss:
+    """
+    An implementation of the image perceptual losses defined in:
+
+    todo: syntax
+    Justin Johnson, Alexandre Alahi, and Li Fei-Fei. 2016. Perceptual losses for real-time
+style transfer and super-resolution. In European Conference on Computer Vision.
+694–711.
+
+    """
+
+    ##
+    # Init
+    model = torchvision.models.vgg16(pretrained=True, progress=True)
+    model.eval()
+
+
